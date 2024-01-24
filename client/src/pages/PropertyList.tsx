ee9942b7bae29b7431189120a3bff39b19c3ad22
itusheve <itamarsando@gmail.com>
2024-01-24 18:35:23 +0200

    update search fields

--- client/src/pages/PropertyList.tsx
+++ client/src/pages/PropertyList.tsx
@@ -1,46 +1,58 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { IPropertyItem } from "../interfaces";
-import ListingJson from '../mocks/propertiesList.json'
+import ListingJson from "../mocks/propertiesList.json";
 import { PropertyItemCard } from "../components/propertyItem";
 import { Header } from "../components/header";
 
+const fetchPropertyList = async () => {
+  const response = await fetch("http://localhost:3000/api/properties", {
+    credentials: "include",
+  });
+  let t = await response.json();
+  return t;
+};
 
-const fetchPropertyList =async ()=>{
-    const response = await  fetch('http://localhost:3000/api/properties',{  credentials: "include"});
-    let t = await response.json();
-    return t;
-}
+export const PropertyList = () => {
+  const [properties, setProperties] = useState<IPropertyItem[]>([]);
+  const getProperties = async () => {
+    const results = await fetchPropertyList();
+    return results;
+  };
 
-export const PropertyList = ()=>{
-    const [properties,setProperties ] =useState<IPropertyItem[]>([]);
-    useEffect(()=>{
-        //go to the server to take the json
-        const getProperties = async ()=>{
-            const results = await  fetchPropertyList();
-            setProperties(results);
-        }
-       getProperties();
-
-    },[]);
-    const SearchProperties = ({query,category}:any)=>{
-        let temp = [...ListingJson].filter((item)=>{
-            return item.address.toLowerCase().includes(query.toLowerCase());
-        })
-        if(category.length){
-            temp = temp.filter((item)=>category.includes(item.category))
-        }
-        setProperties([...temp]);
+  useEffect(() => {
+    //go to the server to take the json
+    const fetchProps = async () => {
+      const results = await getProperties();
+      setProperties(results);
     };
-    return(<>
-    <div className="property-page">
+    fetchProps();
+  }, []);
+  const SearchProperties = async ({ query, category }: any) => {
+    const results = await getProperties();
+    if (query === "" && category === null) {
+      setProperties(results);
+      return;
+    }
+    let temp = [...results].filter((item) => {
+      return item.address.toLowerCase().includes(query.toLowerCase());
+    });
+    if (category.length) {
+      temp = temp.filter((item) => category.includes(item.category));
+    }
+    setProperties([...temp]);
+  };
+  return (
+    <>
+      <div className="property-page">
         <div className="search-wrapper">
-            <Header onSearch={SearchProperties}></Header>
+          <Header onSearch={SearchProperties}></Header>
         </div>
         <div className="property-list-wrapper">
-            {properties.map((item,index)=>{
-                    return <PropertyItemCard {...item} key={index}></PropertyItemCard>
-            })}
+          {properties.map((item, index) => {
+            return <PropertyItemCard {...item} key={index}></PropertyItemCard>;
+          })}
         </div>
-        </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
--- server/src/properties/properties.service.ts
+++ server/src/properties/properties.service.ts
@@ -1,25 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Property } from './entities/property.entity';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOperator, ObjectLiteral, Repository } from "typeorm";
+import { Property } from "./entities/property.entity";
 
 @Injectable()
 export class PropertiesService {
-    /**
-     *
-     */
-    constructor(@InjectRepository(Property)
-    private readonly propertyRepository: Repository<Property>) { }
+  /**
+   *
+   */
+  constructor(
+    @InjectRepository(Property)
+    private readonly propertyRepository: Repository<Property>
+  ) {}
 
-    async getAll() {
-        try {
-            let results = await this.propertyRepository.find({});
-            return results;
-        }
-        catch (e) {
-            throw e;
-        }
+  async getAll() {
+    try {
+      let results = await this.propertyRepository.find({});
+      return results;
+    } catch (e) {
+      throw e;
     }
-   
+  }
 }
-
